bcce900a0f5dfcbf660224bbcfb6f80c14e48aab
ndthong <65402387+thongnd0200@users.noreply.github.com>
2021-10-29 22:58:59 +0700

    add translate API

--- Paint/Draw/urls.py
+++ Paint/Draw/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import drawApi,drawExperimentApi ,randomImage
+from .views import drawApi,drawExperimentApi ,randomImage, translateApi
 
 
 urlpatterns = [
     path('doodle/', drawApi, name='draw'),
     path('doodle-experiment/', drawExperimentApi, name='draw-experiment'),
     path('randimg/', randomImage, name='randimg'),
-
+    path('translate/', translateApi, name='translate'),
 ]
--- Paint/Draw/views.py
+++ Paint/Draw/views.py
@@ -4,7 +4,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from Draw.models import getSampleGuess
-
+import requests
+import json
+from googletrans import Translator
 # Create your views here.
 
 @api_view(['GET', 'POST'])
@@ -15,6 +17,28 @@ def drawApi(request):
 @api_view(['GET', 'POST'])
 def drawExperimentApi(request):
     data = getSampleGuess()
+    return Response(data)
+
+@api_view(['GET'])
+def translateApi(request):
+    keyWord = request.GET.get('keyWord', '')
+    data = {
+        'data':{
+            'keyWord':keyWord,
+            'translations':{
+                'vi':'',
+                'ja':'',
+                'fr':'',
+                'ru':''
+            }
+        }
+    }
+    translator = Translator()
+    data['data']['translations']['vi'] = translator.translate(keyWord, dest='vi').text
+    data['data']['translations']['ja'] = translator.translate(keyWord, dest='ja').text
+    data['data']['translations']['fr'] = translator.translate(keyWord, dest='fr').text
+    data['data']['translations']['ru'] = translator.translate(keyWord, dest='ru').text
+
     return Response(data)
 
 @api_view(['GET'])
