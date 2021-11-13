8d8fa8eddb45bd5aa4e086270ec78386ae2270a4
nvatuan <24392632+nvatuan@users.noreply.github.com>
2021-11-13 13:17:13 +0100

    Change project name to DoodleDict

--- DoodleDict/DoodleDict/wsgi.py
+++ DoodleDict/DoodleDict/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Paint.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DoodleDict.settings')
 
 application = get_wsgi_application()
